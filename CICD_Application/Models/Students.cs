9d22b30b3b240f990c91530b3735385bee672840
sravani <sivadasu88@gmail.com>
2019-03-08 11:29:37 +0530

    Add project files.

---
+++ CICD_Application/Models/Students.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CICD_Application.Models
+{
+    public class Students
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+
+        public string CreateNewStudent()
+        {
+            Students obj = new Students();
+            obj.Id = 1;
+            obj.Name = "sravani";
+            obj.Description = "Description";
+            return obj.ToString();
+        }
+    }
+}
